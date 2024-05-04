325ddf5af2a782bdfe5aff56e69ea74d764b0ae4
bmillettmedia <bryanmillett@cox.net>
2024-05-04 16:26:52 -0700

    changes

--- dom-basics.js
+++ dom-basics.js
@@ -2,9 +2,9 @@ const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript";
 document.body.appendChild(newParagraph);
 
-const newImage = document.createElement("Img");
-newImage.setAttribute("src","htttp://picsum.photos/200");
-newImage.setAttribute("alt","Random image from picsum.");
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Random image from picsum.");
 document.body.appendChild(newImage);
 
 const newDiv = document.createElement("div");
